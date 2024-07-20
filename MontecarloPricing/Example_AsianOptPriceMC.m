a8978a0beb341d70f511b2b28108c0326cfb8d34
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2024-07-20 16:10:01 +0200

    Split examples

--- MontecarloPricing/Example_AsianOptPriceMC.m
+++ MontecarloPricing/Example_AsianOptPriceMC.m
@@ -1,43 +1,6 @@
 clear
 clc
 
-%% UE vanilla option pricing
-% simulation params
-nSims = 1e5;
-
-% option params
-spotPrice = 98;
-strike = 100;
-rate = 0.0001;
-TTM = 1;
-putFlag = false;
-
-%% Merton
-priceModel = 'Merton';
-mertonParams.sigmaD = 0.7;
-mertonParams.lambda = 10;
-mertonParams.muJ    = -0.01;
-mertonParams.sigmaJ = 0.4;
-
-disp('MC price Eu option, Merton model')
-[price,CIprice] = UEOptPriceMC(spotPrice, strike, rate, TTM, putFlag, priceModel, mertonParams, nSims)
-disp('MC price Eu option, Merton model using Antitetich Variable')
-[price,CIprice] = UEOptPriceMC(spotPrice, strike, rate, TTM, putFlag, priceModel, mertonParams, nSims, true)
-
-%% Kou
-priceModel = 'Kou';
-kouParams.sigmaD    = 0.7;
-kouParams.lambda    = 10;
-kouParams.lambdaP   = 15;
-kouParams.lambdaN   = 25;
-kouParams.p         = 0.6;
-
-disp('MC price Eu option, Kou model')
-[price,CIprice] = UEOptPriceMC(spotPrice, strike, rate, TTM, putFlag, priceModel, kouParams, nSims)
-disp('MC price Eu option, Kou model using Antitetich Variable')
-[price,CIprice] = UEOptPriceMC(spotPrice, strike, rate, TTM, putFlag, priceModel, kouParams, nSims, true)
-
-
 %% Asian option pricing
 % simulation params
 nSims = 1e5;
